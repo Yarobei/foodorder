ae447d38679e63b52eff7c0228110a4ba6d798c8
hohterd <hohterd@gmail.com>
2020-03-11 00:36:51 +0300

    add views by role add menu page and page by each category add user and admin by default

---
+++ src/main/java/by/it/academy/foodorder/parent/web/controller/MenuController.java
@@ -0,0 +1,39 @@
+package by.it.academy.foodorder.parent.web.controller;
+
+import by.it.academy.foodorder.parent.model.Category;
+import by.it.academy.foodorder.parent.model.Food;
+import by.it.academy.foodorder.parent.services.interfaces.CategoryService;
+import by.it.academy.foodorder.parent.services.interfaces.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class MenuController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private FoodService foodService;
+
+    @RequestMapping(value = "/menu", method = RequestMethod.GET)
+    public String getMenu(Model model){
+        List<Category> categoryList = categoryService.getAllCategories();
+        model.addAttribute("menu", categoryList);
+        return "menu";
+    }
+
+    @RequestMapping(value = "/menu/{categoryName}")
+    public String getMenuByName(Model model, @PathVariable String categoryName){
+        List<Food> foodList = foodService.gettAllByCategoryName(categoryName);
+        model.addAttribute("menuByCategory", foodList);
+        return "menuList";
+    }
+
+}
